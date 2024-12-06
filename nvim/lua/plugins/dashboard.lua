5be3c2683021e8e1a7390001639f4e6ae02338ed
Chev <Chev@ChevCast.tv>
2024-12-05 18:39:14 -0700

    fix: Update to customize Folke's new dashboard plugin.

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,15 +1,15 @@
 return {
-	"nvimdev/dashboard-nvim",
+	"folke/snacks.nvim",
 	optional = true,
 	opts = function(_, opts)
 		-- Find the color of strings in the current theme and use it as dashboard header color.
 		local string_hl = vim.api.nvim_get_hl(0, { name = "String", link = false })
-		vim.api.nvim_set_hl(0, "DashboardHeader", { fg = string_hl.fg })
+		vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = string_hl.fg })
 
 		-- Define function to pick logo based on working directory.
 		local function pickLogo()
-			local filepath = vim.fn.getcwd() or ""
-			local hostname = vim.fn.hostname() or ""
+			-- local filepath = vim.fn.getcwd() or ""
+			-- local hostname = vim.fn.hostname() or ""
 			-- if string.match(filepath .. hostname, "") then
 			-- 	return [[
 			-- 	]]
@@ -27,7 +27,7 @@ return {
 
 		-- Retrieve relevant logo and set it as dashboard header.
 		local logo = pickLogo()
-		logo = string.rep("\n", 5) .. logo .. "\n\n"
-		opts.config.header = vim.split(logo, "\n")
+		opts.dashboard.preset.header = logo
+		return opts
 	end,
 }
