ac8b72ede436130858bbb1087de884133812d99b
Chev <Chev@ChevCast.tv>
2024-12-14 20:00:46 -0700

    temp: Disabling autopairs to see if I miss it.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,13 +1,13 @@
 return {
 	-- First disable built-in mini.pairs.
-	{
-		"echasnovski/mini.pairs",
-		enabled = false,
-	},
-	-- Enable nvim-autopairs instead.
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {},
-	},
+	-- {
+	-- 	"echasnovski/mini.pairs",
+	-- 	enabled = false,
+	-- },
+	-- -- Enable nvim-autopairs instead.
+	-- {
+	-- 	"windwp/nvim-autopairs",
+	-- 	event = "InsertEnter",
+	-- 	opts = {},
+	-- },
 }
