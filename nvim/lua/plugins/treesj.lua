266c3b2bdd750c06575d5b4da4bb7d7d245e32aa
Chev <Chev@ChevCast.tv>
2024-12-16 12:27:40 -0700

    chore(nvim): plugin updates

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,16 +1,16 @@
 return {
-	"Wansmer/treesj",
-	event = "BufEnter",
-	keys = {
-		{ "<space>rm", "<CMD>TSJToggle<CR>", desc = "Toggle Inline/Block" },
-		{ "<space>rj", "<CMD>TSJJoin<CR>", desc = "Inline" },
-		{ "<space>rs", "<CMD>TSJSplit<CR>", desc = "Block" },
-	},
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
-	opts = {
-		use_default_keymaps = false,
-		max_join_length = 10000,
-	},
+	-- "Wansmer/treesj",
+	-- event = "BufEnter",
+	-- keys = {
+	-- 	{ "<space>rm", "<CMD>TSJToggle<CR>", desc = "Toggle Inline/Block" },
+	-- 	{ "<space>rj", "<CMD>TSJJoin<CR>", desc = "Inline" },
+	-- 	{ "<space>rs", "<CMD>TSJSplit<CR>", desc = "Block" },
+	-- },
+	-- dependencies = {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- },
+	-- opts = {
+	-- 	use_default_keymaps = false,
+	-- 	max_join_length = 10000,
+	-- },
 }
